8fcf6e3785edad5b0b0aca56b08f378aa155317c
Max Streese <max.streese@smarttra.de>
2020-08-25 21:40:17 +0200

    Maybe fix some more

--- .gitpod.tasks.d/prebuild.sh
+++ .gitpod.tasks.d/prebuild.sh
@@ -4,6 +4,9 @@ METALS_DIR="$GITPOD_REPO_ROOT/.metals"
 
 echo "-Dsbt.coursier.home=$METALS_DIR/coursier" >> .jvmopts
 echo "-Dcoursier.cache=$METALS_DIR/coursier" >> .jvmopts
+echo "-Dsbt-dir=$METALS_DIR/sbt" >> .jvmopts
+echo "-Dsbt-boot=$METALS_DIR/sbt/boot" >> .jvmopts
+echo "-Divy=$METALS_DIR/.ivy2" >> .jvmopts
 
 coursier fetch org.scalameta:metals_2.12:0.9.3 --cache=$METALS_DIR/coursier
 
